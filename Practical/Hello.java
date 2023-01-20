378bcfede6c9530acfc3f660852cb91ae74cfc12
SanaShaikh6 <123160974+SanaShaikh6@users.noreply.github.com>
2023-01-20 18:03:09 +0530

    Add files via upload

---
+++ Practical/Hello.java
@@ -0,0 +1,11 @@
+class My{
+    Public static void display(){ //static method 
+        system.out.println("Hi.....");
+    }
+}
+
+class Hello{ //start of class
+    Public static void main (String args[]){  //entry point 
+        System.out.println("Its my first program."); //will print on console
+    }
+} //end of class
