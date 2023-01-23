7c25fc1b9ae7ff6881f8aea873306ad4c8101469
SanaShaikh6 <123160974+SanaShaikh6@users.noreply.github.com>
2023-01-23 18:09:16 +0530

    Add files via upload

---
+++ Practical/VaraibleExample.java
@@ -0,0 +1,11 @@
+class VaraibleExample {
+static int num = 55; //static 
+long num1 =54321980; //instance
+public void display(){ //nonstatic
+char a= 'M'; //local
+double num2= 45.355;
+system.out.println("local variable value : " +a); //printing local variable
+system.out.println("local variable value : " +num2); //
+}
+public static void calc(){
+double
