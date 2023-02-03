221fcf8a4f2542aeca4ca28022201baa973816b7
SanaShaikh6 <123160974+SanaShaikh6@users.noreply.github.com>
2023-02-03 13:57:31 +0530

    Add files via upload

---
+++ Lab/Divisible.java
@@ -0,0 +1,25 @@
+//Write a program to Print numbers from 1-100, which are divisible by 5, and if the number is greater than 40.
+
+public class Divisible {
+
+	public void divBy4GreaterThan40() {
+		
+			for(int i=1;i<=100;i++)
+			{
+				if(i%2==0 && i>40)
+				{
+					System.out.print(i+" ");
+				}
+				
+				
+			}
+		
+	}
+	public static void main(String[] args) {
+		Divisible obj= new Divisible();
+		System.out.println("Numbers from 1-100, which are divisible by 2, and greater than 40 :");
+		obj.divBy4GreaterThan40();
+
+	}
+
+}
