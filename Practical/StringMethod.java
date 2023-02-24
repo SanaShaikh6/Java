035adb7b5d664a4605675a75754c95e1c1f985d5
SanaShaikh6 <123160974+SanaShaikh6@users.noreply.github.com>
2023-02-24 17:30:18 +0530

    Add files via upload

---
+++ Practical/StringMethod.java
@@ -0,0 +1,18 @@
+
+ public class StringMethod{
+	public static void main(String[] args){
+		String a = "Anudip Foundation";
+		System.out.println(a.toUpperCase());
+		System.out.println(a.toLowerCase());
+		System.out.println(a.startsWith("Anu"));
+		System.out.println(a.endsWith("tion"));
+		System.out.println(a.charAt(6));
+		System.out.println(a.length());
+		System.out.println(a.replace("Anudip","Anudeep"));
+		System.out.println(a);
+		
+		int num = 55;
+		String b = String.valueOf(num);
+		System.out.println(b+55);
+		}
+	}
